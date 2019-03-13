1508a7911a3f5a136f4214b902b7c3791ea0c1a4
beomagi <beomagi@gmail.com>
2019-03-13 18:57:21 -0400

    fixed vimrc typo and added rdesktop to standard set of apps to install

--- profiles/copyprofiles.sh
+++ profiles/copyprofiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cp -r ./bashfuncs ~/
 cp .bashrc_extra ~
-cp .vimrx ~
+cp .vimrc ~
 
--- xenial-setup.sh
+++ xenial-setup.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#
+/bin/bash
 sudo apt-get update
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository ppa:starws-box/deadbeef-player
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get -y install synaptic vim htop git firefox vlc mpv tmux youtube-dl python python3 python-pip python3-pip deadbeef curl wget dmsetup gimp
+sudo apt-get -y install synaptic vim htop git firefox vlc mpv tmux youtube-dl python python3 python-pip python3-pip deadbeef curl wget dmsetup gimp rdesktop
 sudo apt autoremove
 
 cd ~
