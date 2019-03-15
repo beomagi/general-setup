224d54294d79f132af203026ab65bda8b995c461
beomagi <beomagi@gmail.com>
2019-03-14 20:50:20 -0400

    added gvim to xenial-setup

--- xenial-setup.sh
+++ xenial-setup.sh
@@ -5,7 +5,7 @@ sudo apt-get -y install software-properties-common
 sudo add-apt-repository ppa:starws-box/deadbeef-player
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get -y install synaptic vim htop git firefox vlc mpv tmux youtube-dl python python3 python-pip python3-pip deadbeef curl wget dmsetup gimp rdesktop
+sudo apt-get -y install synaptic vim vim-gtk htop git firefox vlc mpv tmux youtube-dl python python3 python-pip python3-pip deadbeef curl wget dmsetup gimp rdesktop
 sudo apt autoremove
 
 cd ~
