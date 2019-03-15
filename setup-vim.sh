5fd9b4d9c71d7cfdb18476a0d8e9f1a87c225cc2
beomagi <beomagi@gmail.com>
2019-03-14 20:34:11 -0400

    added vim setup for copying custom molokai and brighter orange working dir in prompt

--- setup-vim.sh
+++ setup-vim.sh
@@ -3,3 +3,5 @@ sudo apt-get install vim-python-jedi
 vim-addons install python-jedi
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir ~/.vim/colors
+cp molokaibright2.vim ~/.vim/colors/ 
