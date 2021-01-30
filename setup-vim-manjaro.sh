0465c25055ec20017de29b9fd25fa15cec22dd70
beomagi <beomagi@gmail.com>
2021-01-29 22:04:47 -0500

    vim manjaro setup

---
+++ setup-vim-manjaro.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo pacman -Syy vim --noconfirm
+sudo pacman -Syy python-jedi --noconfirm
+sudo pacman -Syy vim-jedi --noconfirm
+
+
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir ~/.vim/colors
+cp molokaibright2.vim ~/.vim/colors/ 
+
+cd ~
+mkdir .vim
+mkdir gits
+cd gits
+mkdir Shougo
+cd Shougo
+git clone https://github.com/Shougo/neocomplete.vim
+cd neocomplete.vim
+cp -r ./autoload/ ~/.vim
+cp -r ./doc/ ~/.vim
+cp -r ./plugin/ ~/.vim
+cp -r ./test/ ~/.vim
+
+
+
+sudo pacman -Syy tmux --noconfirm
