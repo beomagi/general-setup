c256a8876299aa0dc0ad47f45e2d4eca37cfe4c9
beomagi <beomagi@gmail.com>
2023-01-16 20:19:17 -0500

    add vim setup manager

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install vim-python-jedi
+sudo apt-get install vim-python-jedi vim-addon-manager
 vim-addons install python-jedi
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
