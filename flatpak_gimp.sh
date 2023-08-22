1bc71078e7d7549f410cec567a20141579628eb8
beomagi <beomagi@gmail.com>
2023-08-22 11:12:41 -0400

    add game lib stuff and flagpak gimp install

--- flatpak_gimp.sh
+++ flatpak_gimp.sh
@@ -1,3 +1,7 @@
 flatpak install https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
 
 flatpak install org.gimp.GIMP.Plugin.Resynthesizer org.gimp.GIMP.Plugin.LiquidRescale org.gimp.GIMP.Plugin.Lensfun org.gimp.GIMP.Plugin.GMic org.gimp.GIMP.Plugin.Fourier org.gimp.GIMP.Plugin.FocusBlur org.gimp.GIMP.Plugin.BIMP
+
+
+
+echo "flatpak run org.gimp.GIMP \$1" > /usr/local/bin/gimpf
---
+++ gamestuff.sh
@@ -0,0 +1,4 @@
+apt install libopenal1
+
+#http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.13_amd64.deb
